6765b66122b26ce8c486ef56e6b28534a23fb5b2
Felipe Mamede <felipebrasilmv@gmail.com>
2022-07-10 23:56:28 -0300

    feat: change react to 18 and fix react tab problem

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -1,6 +1,7 @@
 // REACT
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
 import { ContentContainer } from '../../components/ContentContainer';
 import { TopNavigation } from '../../components/ContentContainer/TopNavigation';
@@ -11,153 +12,177 @@ export function Portfolio() {
       <TopNavigation title="Portfolio" />
       <div className="flex flex-col items-center w-full m-0 p-6 h-[calc(100vh-64px)]">
         <div className="relative w-full min-h-1 px-4">
-          {/* Portfolio Content */}
           <div className="block">
-            <ul className="mb-8 list-none">
-              <li className="inline-block mr-4 text-sm opacity-70 active:opacity-100">
-                <a
-                  className="text-gray-400 active:text-green-500 hover:text-green-500"
-                  data-group="category_all"
-                >
+            <Tabs>
+              <TabList className="mb-8 list-none">
+                <Tab className="inline-block mr-4 text-sm opacity-70 active:opacity-100 text-gray-400 active:text-green-500 hover:text-green-500">
                   All
-                </a>
-              </li>
-              <li className="inline-block mr-4 text-sm opacity-70 active:opacity-100">
-                <a
-                  className="text-gray-400 active:text-green-500 hover:text-green-500"
-                  data-group="category_web"
-                >
+                </Tab>
+                <Tab className="inline-block mr-4 text-sm opacity-70 active:opacity-100 text-gray-400 active:text-green-500 hover:text-green-500">
                   Web
-                </a>
-              </li>
-              <li className="inline-block mr-4 text-sm opacity-70 active:opacity-100">
-                <a
-                  className="text-gray-400 active:text-green-500 hover:text-green-500"
-                  data-group="category_mobile"
-                >
+                </Tab>
+                <Tab className="inline-block mr-4 text-sm opacity-70 active:opacity-100 text-gray-400 active:text-green-500 hover:text-green-500">
                   Mobile
-                </a>
-              </li>
-            </ul>
+                </Tab>
+              </TabList>
+              <TabPanel>
+                <div className="grid w-full justify-center gap-10 grid-cols-portfolio auto-cols-portfolio">
+                  <div className="top-0 left-0 p-2">
+                    <div className="border-4 border-solid border-white w-60 h-60">
+                      <Link to="/portfolio/projects/teste">
+                        <img
+                          src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
+                          alt="Media Project 2"
+                          title=""
+                          className="relative block w-full"
+                        />
+                      </Link>
+                    </div>
 
-            <div className="grid w-full justify-center gap-10 grid-cols-portfolio auto-cols-portfolio ">
-              <div
-                className="top-0 left-0 p-2"
-                data-groups='["category_all", "category_detailed"]'
-              >
-                <div className="border-4 border-solid border-white w-60 h-60">
-                  <Link to="/portfolio/projects/teste">
-                    <img
-                      src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
-                      alt="Media Project 2"
-                      title=""
-                      className="relative block w-full"
-                    />
-                  </Link>
+                    <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
+                      All
+                    </h4>
+                  </div>
+                  <div className="top-0 left-0 p-2">
+                    <div className="border-4 border-solid border-white w-60 h-60">
+                      <a href="#asdada">
+                        <img
+                          src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
+                          alt="Media Project 2"
+                          title=""
+                          className="relative block w-full"
+                        />
+                      </a>
+                    </div>
+
+                    <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
+                      All
+                    </h4>
+                  </div>
+                  <div className="top-0 left-0 p-2">
+                    <div className="border-4 border-solid border-white w-60 h-60">
+                      <a href="#asdada">
+                        <img
+                          src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
+                          alt="Media Project 2"
+                          title=""
+                          className="relative block w-full"
+                        />
+                      </a>
+                    </div>
+
+                    <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
+                      All
+                    </h4>
+                  </div>
                 </div>
+              </TabPanel>
+              <TabPanel>
+                <div className="grid w-full justify-center gap-10 grid-cols-portfolio auto-cols-portfolio">
+                  <div className="top-0 left-0 p-2">
+                    <div className="border-4 border-solid border-white w-60 h-60">
+                      <Link to="/portfolio/projects/teste">
+                        <img
+                          src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
+                          alt="Media Project 2"
+                          title=""
+                          className="relative block w-full"
+                        />
+                      </Link>
+                    </div>
 
-                <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
-                  Detailed Project 2
-                </h4>
-              </div>
-              <div
-                className="top-0 left-0 p-2"
-                data-groups='["category_all", "category_detailed"]'
-              >
-                <div className="border-4 border-solid border-white w-60 h-60">
-                  <a href="#asdada">
-                    <img
-                      src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
-                      alt="Media Project 2"
-                      title=""
-                      className="relative block w-full"
-                    />
-                  </a>
+                    <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
+                      Web
+                    </h4>
+                  </div>
+                  <div className="top-0 left-0 p-2">
+                    <div className="border-4 border-solid border-white w-60 h-60">
+                      <a href="#asdada">
+                        <img
+                          src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
+                          alt="Media Project 2"
+                          title=""
+                          className="relative block w-full"
+                        />
+                      </a>
+                    </div>
+
+                    <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
+                      Web
+                    </h4>
+                  </div>
+                  <div className="top-0 left-0 p-2">
+                    <div className="border-4 border-solid border-white w-60 h-60">
+                      <a href="#asdada">
+                        <img
+                          src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
+                          alt="Media Project 2"
+                          title=""
+                          className="relative block w-full"
+                        />
+                      </a>
+                    </div>
+
+                    <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
+                      Web
+                    </h4>
+                  </div>
                 </div>
+              </TabPanel>
+              <TabPanel>
+                <div className="grid w-full justify-center gap-10 grid-cols-portfolio auto-cols-portfolio">
+                  <div className="top-0 left-0 p-2">
+                    <div className="border-4 border-solid border-white w-60 h-60">
+                      <Link to="/portfolio/projects/teste">
+                        <img
+                          src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
+                          alt="Media Project 2"
+                          title=""
+                          className="relative block w-full"
+                        />
+                      </Link>
+                    </div>
 
-                <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
-                  Detailed Project 2
-                </h4>
-              </div>
-              <div
-                className="top-0 left-0 p-2"
-                data-groups='["category_all", "category_detailed"]'
-              >
-                <div className="border-4 border-solid border-white w-60 h-60">
-                  <a href="#asdada">
-                    <img
-                      src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
-                      alt="Media Project 2"
-                      title=""
-                      className="relative block w-full"
-                    />
-                  </a>
+                    <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
+                      Mobile
+                    </h4>
+                  </div>
+                  <div className="top-0 left-0 p-2">
+                    <div className="border-4 border-solid border-white w-60 h-60">
+                      <a href="#asdada">
+                        <img
+                          src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
+                          alt="Media Project 2"
+                          title=""
+                          className="relative block w-full"
+                        />
+                      </a>
+                    </div>
+
+                    <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
+                      Mobile
+                    </h4>
+                  </div>
+                  <div className="top-0 left-0 p-2">
+                    <div className="border-4 border-solid border-white w-60 h-60">
+                      <a href="#asdada">
+                        <img
+                          src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
+                          alt="Media Project 2"
+                          title=""
+                          className="relative block w-full"
+                        />
+                      </a>
+                    </div>
+
+                    <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
+                      Mobile
+                    </h4>
+                  </div>
                 </div>
-
-                <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
-                  Detailed Project 2
-                </h4>
-              </div>
-              <div
-                className="top-0 left-0 p-2"
-                data-groups='["category_all", "category_detailed"]'
-              >
-                <div className="border-4 border-solid border-white w-60 h-60">
-                  <a href="#asdada">
-                    <img
-                      src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
-                      alt="Media Project 2"
-                      title=""
-                      className="relative block w-full"
-                    />
-                  </a>
-                </div>
-
-                <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
-                  Detailed Project 2
-                </h4>
-              </div>
-              <div
-                className="top-0 left-0 p-2"
-                data-groups='["category_all", "category_detailed"]'
-              >
-                <div className="border-4 border-solid border-white w-60 h-60">
-                  <a href="#asdada">
-                    <img
-                      src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
-                      alt="Media Project 2"
-                      title=""
-                      className="relative block w-full"
-                    />
-                  </a>
-                </div>
-
-                <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
-                  Detailed Project 2
-                </h4>
-              </div>
-              <div
-                className="top-0 left-0 p-2"
-                data-groups='["category_all", "category_detailed"]'
-              >
-                <div className="border-4 border-solid border-white w-60 h-60">
-                  <a href="#asdada">
-                    <img
-                      src="https://lmpixels.com/demo/breezycv/darkfw/1/img/portfolio/2.jpg"
-                      alt="Media Project 2"
-                      title=""
-                      className="relative block w-full"
-                    />
-                  </a>
-                </div>
-
-                <h4 className="block py-4 px-0 text-base relative text-white font-semibold font-display">
-                  Detailed Project 2
-                </h4>
-              </div>{' '}
-            </div>
+              </TabPanel>
+            </Tabs>
           </div>
-          {/*  End of Portfolio Content */}
         </div>
       </div>
     </ContentContainer>
